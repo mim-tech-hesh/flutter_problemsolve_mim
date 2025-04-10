cda89d03ce8e476f0be0eacdae51e03b988dc1ea
mim-tech-hesh <mim15-5924@diu.edu.bd>
2025-04-10 22:21:51 +0600

    Tracker problem-5

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: D()));
+void main() => runApp(E());
 
-class D extends StatelessWidget {
+class E extends StatelessWidget {
+  @override
+  Widget build(BuildContext c) {
+    return MaterialApp(home: F());
+  }
+}
+
+class F extends StatelessWidget {
   @override
   Widget build(BuildContext c) {
     return Scaffold(
-      appBar: AppBar(title: Text('Text Styles')),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Big Bold',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              'Small Grey',
-              style: TextStyle(fontSize: 12, color: Colors.grey),
-            ),
-            Text(
-              'Colored & Medium',
-              style: TextStyle(fontSize: 20, color: Colors.green),
-            ),
-          ],
+      appBar: AppBar(title: Text('First')), // optional, just for better UX
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Go Next'),
+          onPressed:
+              () => Navigator.push(c, MaterialPageRoute(builder: (_) => G())),
         ),
       ),
+    );
+  }
+}
+
+class G extends StatelessWidget {
+  @override
+  Widget build(BuildContext c) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Second')),
+      body: Center(child: Text('Second Screen')),
     );
   }
 }
