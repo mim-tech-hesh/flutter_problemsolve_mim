e3190f6465971f404b0eac93ea07cd883a50d5b0
mim-tech-hesh <mim15-5924@diu.edu.bd>
2025-04-10 22:46:09 +0600

    Tracker problem-6

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(E());
+void main() => runApp(MaterialApp(home: L()));
 
-class E extends StatelessWidget {
+class L extends StatefulWidget {
   @override
-  Widget build(BuildContext c) {
-    return MaterialApp(home: F());
-  }
+  _LState createState() => _LState();
 }
 
-class F extends StatelessWidget {
+class _LState extends State<L> {
+  final _key = GlobalKey<FormState>();
+  String u = '', p = '';
+
   @override
   Widget build(BuildContext c) {
     return Scaffold(
-      appBar: AppBar(title: Text('First')), // optional, just for better UX
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Go Next'),
-          onPressed:
-              () => Navigator.push(c, MaterialPageRoute(builder: (_) => G())),
+      appBar: AppBar(title: Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Form(
+          key: _key,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Username'),
+                onSaved: (v) => u = v ?? '',
+                validator: (v) => v!.isEmpty ? 'Enter username' : null,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                onSaved: (v) => p = v ?? '',
+                validator: (v) => v!.isEmpty ? 'Enter password' : null,
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                child: Text('Login'),
+                onPressed: () {
+                  if (_key.currentState!.validate()) {
+                    _key.currentState!.save();
+                    // Login logic here
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
-    );
-  }
-}
-
-class G extends StatelessWidget {
-  @override
-  Widget build(BuildContext c) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Second')),
-      body: Center(child: Text('Second Screen')),
     );
   }
 }
