e660658d00da831d4adf1c5bc09247ed4fbe4a5f
mim-tech-hesh <mim15-5924@diu.edu.bd>
2025-04-10 21:46:48 +0600

    Tracker problem-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: B()));
+void main() => runApp(MaterialApp(home: C()));
 
-class B extends StatefulWidget {
-  @override
-  _BState createState() => _BState();
-}
-
-class _BState extends State<B> {
-  var txt = 'Press the button';
-
+class C extends StatelessWidget {
   @override
   Widget build(BuildContext c) {
     return Scaffold(
-      appBar: AppBar(title: Text('Task 2')),
-      body: Center(child: Text(txt, style: TextStyle(fontSize: 22))),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(() => txt = 'Button Pressed'),
-        child: Icon(Icons.touch_app),
+      appBar: AppBar(title: Text('List Items')),
+      body: ListView.builder(
+        itemCount: 20,
+        itemBuilder: (ctx, i) => ListTile(title: Text('Item ${i + 1}')),
       ),
     );
   }
