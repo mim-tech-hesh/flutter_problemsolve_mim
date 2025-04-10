e7d4972d694bbf2d3ce832ef842fd5c27f831675
mim-tech-hesh <mim15-5924@diu.edu.bd>
2025-04-10 21:59:48 +0600

    Tracker problem-4

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: C()));
+void main() => runApp(MaterialApp(home: D()));
 
-class C extends StatelessWidget {
+class D extends StatelessWidget {
   @override
   Widget build(BuildContext c) {
     return Scaffold(
-      appBar: AppBar(title: Text('List Items')),
-      body: ListView.builder(
-        itemCount: 20,
-        itemBuilder: (ctx, i) => ListTile(title: Text('Item ${i + 1}')),
+      appBar: AppBar(title: Text('Text Styles')),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Big Bold',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Small Grey',
+              style: TextStyle(fontSize: 12, color: Colors.grey),
+            ),
+            Text(
+              'Colored & Medium',
+              style: TextStyle(fontSize: 20, color: Colors.green),
+            ),
+          ],
+        ),
       ),
     );
   }
