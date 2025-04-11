865e8ffbd5981bb00498acea5168ec1489083968
mim-tech-hesh <mim15-5924@diu.edu.bd>
2025-04-11 15:07:10 +0600

    Tracker problem-11

--- lib/main.dart
+++ lib/main.dart
@@ -4,67 +4,19 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: BtmNav());
+  Widget build(BuildContext ctx) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LocalImg());
   }
 }
 
-class BtmNav extends StatefulWidget {
-  @override
-  _BtmNavState createState() => _BtmNavState();
-}
+// ---------------- Local Asset Image Screen ----------------
 
-class _BtmNavState extends State<BtmNav> {
-  int cur = 0;
-  final pages = [Home(), Srch(), Prof()];
-
+class LocalImg extends StatelessWidget {
   @override
   Widget build(BuildContext ctx) {
     return Scaffold(
-      body: pages[cur],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: cur,
-        onTap: (i) => setState(() => cur = i),
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
-      ),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext ctx) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Home')),
-      body: Center(child: Text('Home Screen', style: TextStyle(fontSize: 24))),
-    );
-  }
-}
-
-class Srch extends StatelessWidget {
-  @override
-  Widget build(BuildContext ctx) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Search')),
-      body: Center(
-        child: Text('Search Screen', style: TextStyle(fontSize: 24)),
-      ),
-    );
-  }
-}
-
-class Prof extends StatelessWidget {
-  @override
-  Widget build(BuildContext ctx) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Profile')),
-      body: Center(
-        child: Text('Profile Screen', style: TextStyle(fontSize: 24)),
-      ),
+      appBar: AppBar(title: Text('Local Image')),
+      body: Center(child: Image.asset('assets/my_img.png', width: 200)),
     );
   }
 }
