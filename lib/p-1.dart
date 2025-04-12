29ea6efe2c5f85e51c76685240e8ffd5d7067d4e
mim-tech-hesh <mim15-5924@diu.edu.bd>
2025-04-12 23:12:40 +0600

    Tracker problem-14

--- lib/p-1.dart
+++ lib/p-1.dart
@@ -3,26 +3,73 @@ import 'package:flutter/material.dart';
 void main() =>
     runApp(MaterialApp(home: MyApp(), debugShowCheckedModeBanner: false));
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var data = List.generate(10, (i) => 'Item ${i + 1}');
+
   @override
   Widget build(BuildContext ctx) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
-        leading: Padding(
-          padding: EdgeInsets.all(8),
-          child: Image.asset(
-            'assets/logo.png',
-            fit: BoxFit.contain,
-          ), // Add your logo here
-        ),
-        title: Text('Custom AppBar'),
-        actions: [
-          IconButton(icon: Icon(Icons.search), onPressed: () {}),
-          IconButton(icon: Icon(Icons.more_vert), onPressed: () {}),
-        ],
+      appBar: AppBar(title: Text('List')),
+      body: ListView.builder(
+        itemCount: data.length,
+        itemBuilder:
+            (c, i) => Dismissible(
+              key: ValueKey(data[i]),
+              background: actBox(
+                Icons.delete,
+                Colors.red,
+                Alignment.centerLeft,
+              ),
+              secondaryBackground: actBox(
+                Icons.edit,
+                Colors.blue,
+                Alignment.centerRight,
+              ),
+              onDismissed: (dir) {
+                if (dir == DismissDirection.endToStart) {
+                  showDialog(
+                    context: ctx,
+                    builder: (_) {
+                      var ctrl = TextEditingController(text: data[i]);
+                      return AlertDialog(
+                        title: Text('Edit'),
+                        content: TextField(controller: ctrl),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              setState(() => data[i] = ctrl.text);
+                              Navigator.pop(ctx);
+                            },
+                            child: Text('Save'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                } else {
+                  setState(() => data.removeAt(i));
+                  ScaffoldMessenger.of(
+                    ctx,
+                  ).showSnackBar(SnackBar(content: Text('Deleted')));
+                }
+              },
+              child: ListTile(title: Text(data[i])),
+            ),
       ),
-      body: Center(child: Text('Body content here')),
+    );
+  }
+
+  Widget actBox(IconData ic, Color clr, Alignment pos) {
+    return Container(
+      color: clr,
+      alignment: pos,
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Icon(ic, color: Colors.white),
     );
   }
 }
