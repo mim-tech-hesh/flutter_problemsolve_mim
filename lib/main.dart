7ed2303f2a34f309f16adf33e69ee4ad8a433d43
mim-tech-hesh <mim15-5924@diu.edu.bd>
2025-04-13 19:57:10 +0600

    Tracker problem-9

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,95 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Home()));
+void main() => runApp(MaterialApp(home: CardList()));
 
-class Home extends StatelessWidget {
+class CardList extends StatelessWidget {
+  final lst = List.generate(
+    5,
+    (i) => {
+      "img": "https://via.placeholder.com/150",
+      "title": "Item $i",
+      "sub": "Subtitle $i",
+      "desc": "This is a description of item $i.",
+    },
+  );
+
   @override
   Widget build(BuildContext ctx) {
     return Scaffold(
-      appBar: AppBar(title: Text("Drawer App")),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(color: Colors.purple),
-              child: Text(
-                'Menu',
-                style: TextStyle(color: Colors.white, fontSize: 22),
-              ),
-            ),
-            ListTile(
-              title: Text("Page A"),
-              onTap:
-                  () => Navigator.push(
-                    ctx,
-                    MaterialPageRoute(builder: (_) => PageA()),
-                  ),
-            ),
-            ListTile(
-              title: Text("Page B"),
-              onTap:
-                  () => Navigator.push(
-                    ctx,
-                    MaterialPageRoute(builder: (_) => PageB()),
-                  ),
-            ),
-          ],
-        ),
+      appBar: AppBar(title: Text("Custom Cards")),
+      body: ListView.builder(
+        itemCount: lst.length,
+        itemBuilder: (c, i) => AniCard(d: lst[i]),
       ),
-      body: Center(child: Text("Main Screen")),
     );
   }
 }
 
-class PageA extends StatelessWidget {
+class AniCard extends StatefulWidget {
+  final Map d;
+  AniCard({required this.d});
+
   @override
-  Widget build(BuildContext ctx) => Scaffold(
-    appBar: AppBar(title: Text("Page A")),
-    body: Center(child: Text("You're on Page A")),
-  );
+  _AniCardState createState() => _AniCardState();
 }
 
-class PageB extends StatelessWidget {
+class _AniCardState extends State<AniCard> {
+  double e = 1.0;
+
   @override
-  Widget build(BuildContext ctx) => Scaffold(
-    appBar: AppBar(title: Text("Page B")),
-    body: Center(child: Text("You're on Page B")),
-  );
+  Widget build(BuildContext ctx) {
+    return GestureDetector(
+      onTapDown: (_) => setState(() => e = 0.95),
+      onTapUp: (_) => setState(() => e = 1.0),
+      onTapCancel: () => setState(() => e = 1.0),
+      child: AnimatedScale(
+        scale: e,
+        duration: Duration(milliseconds: 200),
+        child: Card(
+          margin: EdgeInsets.all(10),
+          elevation: 5,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(12),
+            child: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.network(
+                    widget.d["img"],
+                    width: 80,
+                    height: 80,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                SizedBox(width: 12),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.d["title"],
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        widget.d["sub"],
+                        style: TextStyle(color: Colors.grey[600]),
+                      ),
+                      SizedBox(height: 6),
+                      Text(widget.d["desc"], style: TextStyle(fontSize: 13)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
