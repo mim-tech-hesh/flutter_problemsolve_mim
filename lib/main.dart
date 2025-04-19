02d23f379d6e83d2d7d21f48df9668e4b7a0ae27
mim-tech-hesh <mim15-5924@diu.edu.bd>
2025-04-19 20:44:01 +0600

    Tracker problem-19

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,73 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 
-void main() =>
-    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: ProfilePg()));
-
-class ProfilePg extends StatefulWidget {
+class SlideDrawer extends StatefulWidget {
   @override
-  _ProfilePgState createState() => _ProfilePgState();
+  _SlideDrawerState createState() => _SlideDrawerState();
 }
 
-class _ProfilePgState extends State<ProfilePg> {
-  File? img;
-  final pkr = ImagePicker();
+class _SlideDrawerState extends State<SlideDrawer>
+    with SingleTickerProviderStateMixin {
+  late AnimationController ctrl;
+  late Animation<Offset> anim;
 
-  Future<void> pickImg() async {
-    try {
-      final x = await pkr.pickImage(source: ImageSource.gallery);
-      if (x != null) setState(() => img = File(x.path));
-    } catch (e) {
-      print("Error: $e");
-    }
+  @override
+  void initState() {
+    super.initState();
+    ctrl = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+    anim = Tween(
+      begin: Offset(-1, 0),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: ctrl, curve: Curves.easeInOut));
   }
+
+  void toggle() => ctrl.isDismissed ? ctrl.forward() : ctrl.reverse();
 
   @override
   Widget build(BuildContext ctx) {
     return Scaffold(
-      appBar: AppBar(title: Text("Profile")),
-      body: Center(
-        child: GestureDetector(
-          onTap: pickImg,
-          child: CircleAvatar(
-            radius: 60,
-            backgroundColor: Colors.grey[300],
-            backgroundImage: img != null ? FileImage(img!) : null,
-            child:
-                img == null
-                    ? Icon(Icons.person, size: 50, color: Colors.grey[700])
-                    : null,
+      appBar: AppBar(title: Text("Drawer Ani")),
+      body: Stack(
+        children: [
+          Center(child: ElevatedButton(onPressed: toggle, child: Text("Menu"))),
+          SlideTransition(
+            position: anim,
+            child: Container(
+              width: 250,
+              height: double.infinity,
+              color: Colors.grey[800],
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Home",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Profile",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  Spacer(),
+                  IconButton(
+                    icon: Icon(Icons.close, color: Colors.white),
+                    onPressed: toggle,
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    ctrl.dispose();
+    super.dispose();
   }
 }
