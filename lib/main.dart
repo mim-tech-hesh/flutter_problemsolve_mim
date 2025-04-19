628487c88f55130119c2d6c8a308e37fed50b228
mim-tech-hesh <mim15-5924@diu.edu.bd>
2025-04-19 20:41:08 +0600

    Tracker problem-17

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,43 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-void main() => runApp(MySet());
+void main() =>
+    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: ProfilePg()));
 
-class MySet extends StatefulWidget {
+class ProfilePg extends StatefulWidget {
   @override
-  _MySetState createState() => _MySetState();
+  _ProfilePgState createState() => _ProfilePgState();
 }
 
-class _MySetState extends State<MySet> {
-  bool dark = false;
+class _ProfilePgState extends State<ProfilePg> {
+  File? img;
+  final pkr = ImagePicker();
+
+  Future<void> pickImg() async {
+    try {
+      final x = await pkr.pickImage(source: ImageSource.gallery);
+      if (x != null) setState(() => img = File(x.path));
+    } catch (e) {
+      print("Error: $e");
+    }
+  }
 
   @override
   Widget build(BuildContext ctx) {
-    return MaterialApp(
-      theme: dark ? ThemeData.dark() : ThemeData.light(),
-      home: Scaffold(
-        appBar: AppBar(title: Text("Q17")),
-        body: ListTile(
-          title: Text("Dark Mode"),
-          trailing: Switch(
-            value: dark,
-            onChanged: (v) => setState(() => dark = v),
+    return Scaffold(
+      appBar: AppBar(title: Text("Profile")),
+      body: Center(
+        child: GestureDetector(
+          onTap: pickImg,
+          child: CircleAvatar(
+            radius: 60,
+            backgroundColor: Colors.grey[300],
+            backgroundImage: img != null ? FileImage(img!) : null,
+            child:
+                img == null
+                    ? Icon(Icons.person, size: 50, color: Colors.grey[700])
+                    : null,
           ),
         ),
       ),
